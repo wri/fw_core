4d82e74b0070ff76118aeab677314ff6c6b5f158
Edward Martin <edward@3sidedcube.com>
2022-10-20 17:09:47 +0100

    add username to user on auth

--- src/assignments/assignments.service.ts
+++ src/assignments/assignments.service.ts
@@ -17,36 +17,43 @@ const allowedKeys = [
   'status',
   'areaId',
   'templateId',
-  'teamIds'
-]
+  'teamIds',
+];
 
 @Injectable()
 export class AssignmentsService {
-
   constructor(
-    @InjectModel(Assignment.name, 'formsDb') private assignmentModel: Model<AssignmentDocument>,
+    @InjectModel(Assignment.name, 'formsDb')
+    private assignmentModel: Model<AssignmentDocument>,
     private readonly teamsService: TeamsService,
     private readonly areasService: AreasService,
-  ) { }
+  ) {}
 
-  async create(assignment: CreateAssignmentDto, user: IUser): Promise<AssignmentDocument> {
-
+  async create(
+    assignment: CreateAssignmentDto,
+    user: IUser,
+  ): Promise<AssignmentDocument> {
     // get number of assignments in area for assignment name code
-    let count = await this.assignmentModel.count({createdBy: user.id})
-    const area = await this.areasService.getAreaMICROSERVICE(assignment.areaId)
+    const count = await this.assignmentModel.count({ createdBy: user.id });
+    const area = await this.areasService.getAreaMICROSERVICE(assignment.areaId);
 
-    if(!area) throw new HttpException('Area does not exist', HttpStatus.NOT_FOUND)
+    if (!area)
+      throw new HttpException('Area does not exist', HttpStatus.NOT_FOUND);
 
-
-    const userInitials = user.name ? user.name.split(" ").map(n=>n[0]).join("") : "null"
+    const userInitials = user.name
+      ? user.name
+          .split(' ')
+          .map((n) => n[0])
+          .join('')
+      : 'null';
 
     const newAssignment = {
       ...assignment,
       createdBy: user.id,
-      name: `${userInitials}-${String(count+1).padStart(4,"0")}`
-    }
+      name: `${userInitials}-${String(count + 1).padStart(4, '0')}`,
+    };
 
-    const assignmentToSave = new this.assignmentModel(newAssignment)
+    const assignmentToSave = new this.assignmentModel(newAssignment);
     return await assignmentToSave.save();
   }
 
@@ -55,46 +62,59 @@ export class AssignmentsService {
   }
 
   async findUser(userId: string): Promise<AssignmentDocument[]> {
-    return await this.assignmentModel.find({monitors: userId});
+    return await this.assignmentModel.find({ monitors: userId });
   }
 
   async findTeams(userId: string): Promise<AssignmentDocument[]> {
-
     // get all user team ids
-    const teams = await this.teamsService.findAllByUserId(userId)
-    const teamIds = teams.map(team => team.id)
+    const teams = await this.teamsService.findAllByUserId(userId);
+    const teamIds = teams.map((team) => team.id);
 
-    return await this.assignmentModel.find({teamIds: {$in: teamIds}})
+    return await this.assignmentModel.find({ teamIds: { $in: teamIds } });
   }
 
   async findOpen(userId: string): Promise<AssignmentDocument[]> {
-    return await this.assignmentModel.find({monitors: userId, status: {$in: ["open", "on hold"]}});
+    return await this.assignmentModel.find({
+      monitors: userId,
+      status: { $in: ['open', 'on hold'] },
+    });
   }
 
-  async findAreas(userId: string, areaId: string): Promise<AssignmentDocument[]> {
-
+  async findAreas(
+    userId: string,
+    areaId: string,
+  ): Promise<AssignmentDocument[]> {
     // get all user team ids
-    const teams = await this.teamsService.findAllByUserId(userId)
-    const teamIds = teams.map(team => team.id)
+    const teams = await this.teamsService.findAllByUserId(userId);
+    const teamIds = teams.map((team) => team.id);
 
-    return await this.assignmentModel.find({teamIds: {$in: teamIds}, areaId})
+    return await this.assignmentModel.find({
+      teamIds: { $in: teamIds },
+      areaId,
+    });
   }
 
-  async update(id: string, updateAssignmentDto: UpdateAssignmentDto): Promise<AssignmentDocument> {
-    let assignmentToUpdate = await this.assignmentModel.findOne({_id: new mongoose.Types.ObjectId(id)})
+  async update(
+    id: string,
+    updateAssignmentDto: UpdateAssignmentDto,
+  ): Promise<AssignmentDocument> {
+    const assignmentToUpdate = await this.assignmentModel.findOne({
+      _id: new mongoose.Types.ObjectId(id),
+    });
 
-    for(const [key, value] of Object.entries(updateAssignmentDto)) {
-      if(!allowedKeys.includes(key)) continue;
+    for (const [key, value] of Object.entries(updateAssignmentDto)) {
+      if (!allowedKeys.includes(key)) continue;
       assignmentToUpdate[key] = value;
     }
 
     const updatedAssignment = await assignmentToUpdate.save();
     return updatedAssignment;
-
   }
 
   async remove(id: string): Promise<void> {
-    await this.assignmentModel.findOneAndDelete({_id: new mongoose.Types.ObjectId(id)})
-    return null
+    await this.assignmentModel.findOneAndDelete({
+      _id: new mongoose.Types.ObjectId(id),
+    });
+    return null;
   }
 }
--- src/assignments/tests/assignments.constants.ts
+++ src/assignments/tests/assignments.constants.ts
@@ -1,16 +1,16 @@
-import areaConstants from "../../areas/test/area.constants"
-import ROLES from "../../common/testConstants"
+import areaConstants from '../../areas/test/area.constants';
+import ROLES from '../../common/testConstants';
 
 export default {
   defaultAssignment: {
     location: {},
     priority: 1,
     monitors: [ROLES.USER.id],
-    notes: "some notes",
-    status: "open",
-    alert: "some alert",
+    notes: 'some notes',
+    status: 'open',
+    alert: 'some alert',
     areaId: areaConstants.testArea.id,
-    templateId: "someTemplateId",
+    templateId: 'someTemplateId',
     teamIds: [],
-  }
-}
+  },
+};
