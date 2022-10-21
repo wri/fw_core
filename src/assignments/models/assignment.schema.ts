031a7cd28634d173da2d05aad6ec11873f1baf7a
Edward Martin <edward@3sidedcube.com>
2022-10-21 11:44:22 +0100

    test

--- src/assignments/models/assignment.schema.ts
+++ src/assignments/models/assignment.schema.ts
@@ -1,72 +1,68 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
-import * as mongoose from "mongoose";
-import { IGeojson } from "../../areas/models/area.entity";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
 
 export interface IAssignment {
-name: string;
-location?: any;
-priority: number;
-monitors: string[];
-notes: string;
-status: string;
-alert?: string;
-areaId: string;
-templateId: string;
-teamIds: string[];
-createdAt: Date;
-createdBy?: string;
-areaName?: string;
+  name: string;
+  location?: any;
+  priority: number;
+  monitors: string[];
+  notes: string;
+  status: string;
+  alert?: string;
+  areaId: string;
+  templateId: string;
+  teamIds: string[];
+  createdAt: Date;
+  createdBy?: string;
+  areaName?: string;
 }
 
 @Schema()
 export class Assignment {
+  @Prop({ required: true })
+  name: string;
 
+  @Prop({ required: false })
+  location: mongoose.Schema.Types.Mixed;
 
-    @Prop({required: true})
-    name: string;
+  @Prop({ required: true })
+  priority: number;
 
-    @Prop({required: false})
-    location: mongoose.Schema.Types.Mixed;
+  @Prop({ required: true })
+  monitors: string[];
 
-    @Prop({required: true})
-    priority: number;
+  @Prop({ required: true })
+  notes: string;
 
-    @Prop({required: true})
-    monitors: string[];
+  @Prop({ required: true })
+  status: string;
 
-    @Prop({required: true})
-    notes: string;
+  @Prop({ required: false })
+  alert: string;
 
-    @Prop({required: true})
-    status: string;
+  @Prop({ required: true })
+  areaId: string;
 
-    @Prop({required: false})
-    alert: string;
+  @Prop({ required: true })
+  templateId: string;
 
-    @Prop({required: true})
-    areaId: string;
+  @Prop({ required: true })
+  teamIds: string[];
 
-    @Prop({required: true})
-    templateId: string;
-    
-    @Prop({required: true})
-    teamIds: string[];
+  @Prop({ required: false })
+  areaName: string;
 
-    @Prop({required: false})
-    areaName: string;
+  @Prop({ required: true })
+  createdBy: string;
 
-    @Prop({required: true})
-    createdBy: string;
-
-    @Prop({required: true, default: Date.now()})
-    createdAt: Date;
-
+  @Prop({ required: true, default: Date.now() })
+  createdAt: Date;
 }
 
 export interface AssignmentDocument extends IAssignment, mongoose.Document {}
 
 export interface IAssignmentResponse {
-  data: any
+  data: any;
 }
 
 export const AssignmentSchema = SchemaFactory.createForClass(Assignment);
