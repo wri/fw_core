86e09318cf5b81c7a4559d3ed9a0a9b280962ce7
Edward Martin <edward@3sidedcube.com>
2022-11-01 11:38:21 +0000

    sample rate

--- src/main.ts
+++ src/main.ts
@@ -26,9 +26,7 @@ async function bootstrap() {
   Sentry.init({
     dsn: 'https://a6b18ef7ce1d43298127081511289af7@o163691.ingest.sentry.io/4504083459211264',
     environment: process.env.NODE_ENV,
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
+    sampleRate: 1,
   });
 
   app.use(async (req, res, next) => {
