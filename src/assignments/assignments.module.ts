912a3935e6b0d77e689979a18848cc1789733af4
Burhanuddin Salim <salim@3sidedcube.com>
2022-11-28 12:52:05 +0400

    Change assignment module imports

--- src/assignments/assignments.module.ts
+++ src/assignments/assignments.module.ts
@@ -3,15 +3,12 @@ import { AssignmentsService } from './assignments.service';
 import { AssignmentsController } from './assignments.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Assignment, AssignmentSchema } from './models/assignment.schema';
-import { TeamsService } from '../teams/services/teams.service';
-import { TeamSchema } from '../teams/models/team.schema';
-import { TeamMembersService } from '../teams/services/teamMembers.service';
-import { TeamMemberSchema } from '../teams/models/teamMember.schema';
 import { UserService } from '../common/user.service';
-import { ConfigService } from '@nestjs/config';
 import { AreasModule } from '../areas/modules/areas.module';
 import { S3Service } from '../answers/services/s3Service';
 import { TemplatesModule } from '../templates/templates.module';
+import { TeamsModule } from '../teams/modules/teams.module';
+import { TeamMembersModule } from '../teams/modules/teamMembers.module';
 
 @Module({
   imports: [
@@ -19,26 +16,13 @@ import { TemplatesModule } from '../templates/templates.module';
       [{ name: Assignment.name, schema: AssignmentSchema }],
       'formsDb',
     ),
-    MongooseModule.forFeature(
-      [{ name: 'gfwteams', schema: TeamSchema }],
-      'teamsDb',
-    ),
-    MongooseModule.forFeature(
-      [{ name: 'teamuserrelations', schema: TeamMemberSchema }],
-      'teamsDb',
-    ),
     AreasModule,
     forwardRef(() => TemplatesModule),
+    TeamsModule,
+    TeamMembersModule,
   ],
   controllers: [AssignmentsController],
-  providers: [
-    AssignmentsService,
-    TeamsService,
-    TeamMembersService,
-    UserService,
-    ConfigService,
-    S3Service,
-  ],
+  providers: [AssignmentsService, UserService, S3Service],
   exports: [AssignmentsService],
 })
 export class AssignmentsModule {}
