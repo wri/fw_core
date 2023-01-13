a121a80d41865b5dd0ca70a10348362f96c20117
Burhanuddin Salim <salim@3sidedcube.com>
2023-01-13 13:57:54 +0000

    Get only published public templates

--- src/templates/templates.service.ts
+++ src/templates/templates.service.ts
@@ -84,9 +84,15 @@ export class TemplatesService extends BaseService<
     projection: (keyof TemplateDocument)[];
   }): Promise<TemplateDocument[]> {
     if (opts?.projection) {
-      return this.model.find({ public: true }, { projection: opts.projection });
+      return this.model.find(
+        { public: true, status: ETemplateStatus.UNPUBLISHED },
+        { projection: opts.projection },
+      );
     }
-    return this.model.find({ public: true });
+    return this.model.find({
+      public: true,
+      status: ETemplateStatus.UNPUBLISHED,
+    });
   }
 
   async deleteAllVersions(id, editGroupId): Promise<void> {
