9e1c6bc707a3ca0d3571a53bf909f2baf96c67f2
edward3sc <edward3sc@users.noreply.github.com>
2025-02-19 14:23:45 +0000

    auto commit: apply eslint fixes

--- src/answers/answers.controller.ts
+++ src/answers/answers.controller.ts
@@ -171,7 +171,7 @@ export class AnswersController {
           `${question.label[answer.language]} (${question.name}) required`,
         );
 
-      this.logger.log("ANSWERING QUESTION", name)
+      this.logger.log('ANSWERING QUESTION', name);
 
       await addResponseOrFail(question);
 
