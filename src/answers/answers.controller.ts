895df4226d906c2b7aec14fda3a9a11b79d3a0a8
Edward Martin <edward@3sidedcube.com>
2025-02-19 15:00:13 +0000

    debug file uploading

--- src/answers/answers.controller.ts
+++ src/answers/answers.controller.ts
@@ -60,7 +60,7 @@ export class AnswersController {
     this.logger.log('CREATING REPORT');
     this.logger.log('FIELDS', JSON.stringify(fields));
     this.logger.log(
-      'FILE ARRAY',
+      'FILE ARRAY', fileArray?.length,
       fileArray?.map((file) => file.fieldname),
     );
     this.logger.log(
