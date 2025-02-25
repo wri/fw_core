e0fcf2cf3d25b9194114a3ab54438ac1889f1a4d
Edward Martin <edward@3sidedcube.com>
2025-02-25 09:54:28 +0000

    debug(answers): log request

--- src/answers/answers.controller.ts
+++ src/answers/answers.controller.ts
@@ -55,7 +55,7 @@ export class AnswersController {
   ) {
     const { template } = request;
 
-    this.logger.log('REQUEST', JSON.stringify(request));
+    this.logger.log('REQUEST', JSON.stringify(request.readableObjectMode));
 
     const userPosition = fields.userPosition ?? [];
 
